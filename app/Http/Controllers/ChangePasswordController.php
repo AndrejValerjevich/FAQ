7f21f0dbfcf6663b0ff6746ba8ab1ec11dea273d
AndrejValerjevich <andrejdenisiuk@yandex.ru>
2017-07-03 01:35:47 +0500

    user, 25% admin, auth

---
+++ app/Http/Controllers/ChangePasswordController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Андрей Валерьевич
+ * Date: 03.07.2017
+ * Time: 0:57
+ */
+
+namespace FAQ\Http\Controllers;
+
+
+class ChangePasswordController
+{
+
+}
